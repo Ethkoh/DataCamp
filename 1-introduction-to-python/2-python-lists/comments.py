fcabf846311d7923b91a8c6885524c3176aa1cd4
Ethan Koh <ethan.kohth@gmail.com>
2019-07-12 10:22:57 +0800

    Create comments.py

---
+++ 1-introduction-to-python/2-python-lists/comments.py
@@ -0,0 +1,10 @@
+"""
+The ; sign is used to place commands on the same line. The following two code chunks are equivalent:
+"""
+
+# Same line
+command1; command2
+
+# Separate lines
+command1
+command2
