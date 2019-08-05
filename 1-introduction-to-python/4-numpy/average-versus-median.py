00c75ec8998059e569ea144ec53a4c33f9a70d02
Ethan Koh <ethan.kohth@gmail.com>
2019-08-06 01:36:16 +0800

    Update average-versus-median.py

--- 1-introduction-to-python/4-numpy/average-versus-median.py
+++ 1-introduction-to-python/4-numpy/average-versus-median.py
@@ -27,3 +27,7 @@ print(np.mean(np_height_in))
 
 # Print out the median of np_height_in
 print(np.median(np_height_in))
+
+"""
+An average height of 1586 inches, that doesn't sound right, does it? However, the median does not seem affected by the outliers: 74 inches makes perfect sense. It's always a good idea to check both the median and the mean, to get an idea about the overall distribution of the entire dataset.
+"""
